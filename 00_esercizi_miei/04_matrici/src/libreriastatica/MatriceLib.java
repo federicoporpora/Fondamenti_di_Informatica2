96eaccd779fd65a3f315a2df22caf5f06e029600
Federico Porpora <porporafederico@gmail.com>
2024-03-10 19:11:43 +0100

    matrice lib da provare

--- 00_esercizi_miei/04_matrici/src/libreriastatica/MatriceLib.java
+++ 00_esercizi_miei/04_matrici/src/libreriastatica/MatriceLib.java
@@ -1,5 +1,36 @@
 package libreriastatica;
 
-public class MatriceLib {
+import java.util.StringJoiner;
 
+public class MatriceLib {
+    public static double[][] sommaMatrici(double[][] a, double[][] b) {
+        double[][] res = new double[a.length][b.length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                res[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        return res;
+    }
+    public static double[][] prodottoMatrici(double[][] a, double[][] b) {
+        double[][] res = new double[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                for (int k = 0; k < a[0].length; k++) {
+                    res[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return res;
+    }
+    public static void stampaMatrice(double[][] a) {
+        StringJoiner esternoJoiner = new StringJoiner(",\n", "{", "}");
+        StringJoiner internoJoiner = new StringJoiner(", ", "{", "}");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                internoJoiner.add(Double.toString(a[i][j]));
+            }
+            esternoJoiner.add(internoJoiner.toString());
+        }
+    }
 }
